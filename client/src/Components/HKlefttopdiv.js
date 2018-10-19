378a8e22c9d1353c0546db5b436f6a30192eee29
Vivek <visingh0@in.ibm.com>
2018-10-19 10:51:54 +0530

    Added iframe for Radheshyam Prabhu lectures and Prabhupada lectures

--- client/src/Components/HKlefttopdiv.js
+++ client/src/Components/HKlefttopdiv.js
@@ -2,12 +2,15 @@ import React from "react";
 import "../assets/css/stylesheet.css"
 
 export default class HKlefttopdiv extends React.Component {
+    onIframeClick(){debugger
+        window.open("http://radheshyamdas.com/")
+    }
     render(){
         return (
-            <div id="HKlefttopdiv">
-                <div>{/* HK3 !!!! */}
-                    <a href="http://radheshyamdas.com/"> RadheShyam PrabhuJi Lectures</a>
-                </div>
+            <div id="HKlefttopdiv" onClick={this.onIframeClick.bind(this)}>
+                <iframe title="Radheshyam Prabhu Website" className="iframe" src="http://radheshyamdas.com/" onClick={this.onIframeClick.bind(this)}>{/* HK3 !!!! */}
+                    {/* <a href="http://radheshyamdas.com/"> RadheShyam PrabhuJi Lectures</a> */}
+                </iframe>
             </div>
         )
     }
---
+++ client/src/Components/HKlogin.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import "../assets/css/stylesheet.css"
+
+export default class LoginPage extends Component{
+    render(){
+        return(
+            <div style={{height:"84%",width:"96%",backgroundColor:"blue"}}>
+                HK !!!!
+            </div>
+        )
+    }
+}
